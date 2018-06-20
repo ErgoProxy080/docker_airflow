f2bdbd58dbf2e34b2af9e6b152f51f373eabddf1
Michel Maiorana <michel.j.maiorana@gmail.com>
2018-06-20 00:29:07 -0400

    variable fixes for config, entrypoint and command fixes

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-: ${POSTGRES_USER:="airflow"}
-: ${POSTGRES_PASS:="airflow"}
-: ${POSTGRES_HOST:="postgres"}
-: ${POSTGRES_PORT:="5432"}
+: "${POSTGRES_USER:="airflow"}"
+: "${POSTGRES_PASS:="airflow"}"
+: "${POSTGRES_HOST:="postgres"}"
+: "${POSTGRES_PORT:="5432"}"
 
-: ${RABBITMQ_DEFAULT_USER:="airflow"}
-: ${RABBITMQ_DEFAULT_PASS:="airflow"}
-: ${RABBITMQ_DEFAULT_VHOST:="airflow"}
-: ${RABBITMQ_PORT:="5672"}
+: "${RABBITMQ_DEFAULT_USER:="airflow"}"
+: "${RABBITMQ_DEFAULT_PASS:="airflow"}"
+: "${RABBITMQ_DEFAULT_VHOST:="airflow"}"
+: "${RABBITMQ_PORT:="5672"}"
 
-: "${AIRFLOW__CORE__FERNET__KEY:=${FERNET_KEY:="$(python3 /usr/local/bin/fernet.py)"}}"
-: "${AIRFLOW__CORE__EXECUTOR:=${AIRFLOW_EXECUTOR:-Sequential}Executor}"
+: "${AIRFLOW__CORE__FERNET__KEY:="$(python3 /usr/local/bin/fernet.py)"}"
+: "${AIRFLOW__CORE__EXECUTOR:="SequentialExecutor"}"
 
 
-if ["$AIRFLOW__CORE__EXECUTOR" == "Celery" ]; then
+if [ "$AIRFLOW__CORE__EXECUTOR" == "CeleryExecutor" ]; then
     AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASS}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
     AIRFLOW__CELERY__BROKER_URL="amqp://${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}:${RABBITMQ_PORT}/${RABBITMQ_DEFAULT_VHOST}"
     AIRFLOW__CELERY__CELERY_RESULT_BACKEND="$AIRFLOW__CORE__SQL_ALCHEMY_CONN"
@@ -29,20 +29,22 @@ export \
 
 case "$1" in
   webserver)
-    exec airflow "$?"
+    airflow initdb
+    sleep 10
+    exec airflow "$@"
     ;;
   scheduler)
-    initdb
+    airflow initdb
     sleep 10
-    exec airflow "$?"
+    exec airflow "$@"
     ;;
   worker)
-    exec airflow "$?"
+    exec airflow "$@"
     ;;
   flower)
-    exec airflow "$?"
+    exec airflow "$@"
     ;;
   *)
-    exec "$?"
+    exec "$@"
     ;;
 esac
